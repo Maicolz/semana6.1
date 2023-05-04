ba7581412738d7deebffcbbb01d55d3491d47a97
Maicolz <113947903+Maicolz@users.noreply.github.com>
2023-05-04 18:09:47 -0500

    Delete AVANCE4-HTML directory

--- AVANCE4-HTML/desvio.php
+++
@@ -1,9 +0,0 @@
-<?php
-$nombre = $_REQUEST['nombres'];
-$apellido = $_REQUEST['apellidos'];
-$edad = $_REQUEST['edad'];
-$lugar = $_REQUEST['lugar'];
-
-echo "<h1> ATU-DESVIOS POR OBRAS </h1>";
-echo $nombre." ". $apellido." , gracias por dejar tu aviso. Ahora todos los usuarios sabrán que en ".$lugar. " hay un desvío por obras.";
-?>
